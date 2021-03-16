a1cc2e63c520b96eaa186135bfc393161dd08aff
Nahuel Fedyszyn <32500578+NahuelFedyszyn@users.noreply.github.com>
2021-03-16 09:29:05 -0300

    1st commit 16/03

---
+++ app.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset; //This variable indicates if we are going up or down in the page
+window.onscroll = function() { //This function hides the header if we are going down, and shows it if we are going up
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-10vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
